16fab3fd34edbc6440869cd983d6d33f9ad4195b
Luis Eduardo Henriquez Contreras <lhenriqu@bancolombia.com.co>
2022-04-21 13:48:28 -0400

    Add constructors, getter and setter to Employee entity

--- src/com/henz/kitchen/simulator/entities/Employee.java
+++ src/com/henz/kitchen/simulator/entities/Employee.java
@@ -8,4 +8,35 @@ public class Employee {
 
     private EmployeeStatus status;
 
+    public Employee() {
+    }
+
+    public Employee(String name, EmployeeStatus status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EmployeeStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(EmployeeStatus status) {
+        this.status = status;
+    }
 }
